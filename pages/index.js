460e1534c22c18947cedfc907d6616b7948aad6c
Ricado Bonin <ricado@brittes.com.br>
2021-05-16 23:24:03 -0300

    feat: Tratar errors campos

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,25 @@ import { useState } from 'react';
 const INITIAL_STATE = {
   name: '',
   email: '',
-  password: '',
-  errors: {
-    error_name: false,
-    error_email: false,
-    error_password: false
-  },
-  errors_msg: {
-    error_name: '',
-    error_email: '',
-    error_password: ''
-  }
+  password: ''
 };
 
 export default function Home() {
   const [state, setState] = useState(INITIAL_STATE);
-  //const { error_name, error_email, error_password } = state.errors;
+  const [errorName, setErrorName] = useState({
+    error_name: false,
+    error_msg: ''
+  });
+  const [errorEmail, setErrorEmail] = useState({
+    error_email: false,
+    error_msg: ''
+  });
+  const [errorPassword, setErrorPassword] = useState({
+    error_password: false,
+    error_msg: ''
+  });
+  const [success, setSuccess] = useState(false);
+
   const handlerChange = e => {
     const { name, value } = e.target;
 
@@ -30,20 +33,17 @@ export default function Home() {
   //validar nome
 
   const validateName = name => {
-    if (name.length == 0) {
-      setState({
-        ...state,
-        errors: { ...state.errors, error_name: true },
-        errors_msg: {
-          ...state.errors,
-          error_name: 'Empty field. Please fill in the field with your name.'
-        }
+    if (name.length === 0) {
+      setErrorName({
+        ...errorName,
+        error_name: true,
+        error_msg: 'Campo vazio. Por favor, preencha o campo com seu nome.'
       });
     } else {
-      setState({
-        ...state,
-        errors: { ...state.errors, error_name: false },
-        errors_msg: { ...state.errors, error_name: '' }
+      setErrorName({
+        ...errorName,
+        error_name: false,
+        error_msg: ''
       });
     }
   };
@@ -51,14 +51,39 @@ export default function Home() {
   //validade email
   const validadeEmail = email => {
     const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    console.log('>>>', reg.test(email));
+    if (!reg.test(email)) {
+      setErrorEmail({
+        ...errorEmail,
+        error_email: true,
+        error_msg: 'Erro. Por favor, adicione um email válido.'
+      });
+    } else {
+      setErrorEmail({
+        ...errorEmail,
+        error_email: false,
+        error_msg: ''
+      });
+    }
   };
 
   //validar senha
 
   const validadePassword = password => {
     const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
-    console.log('>>>', reg.test(password));
+    if (!reg.test(password)) {
+      setErrorPassword({
+        ...errorPassword,
+        error_password: true,
+        error_msg:
+          'Erro. Sua senha deve conter um símbolo, um número, uma letra maiúscula, deve conter pelo menos 8 caracteres e não pode conter caracteres iguais.'
+      });
+    } else {
+      setErrorPassword({
+        ...errorPassword,
+        error_password: false,
+        error_msg: ''
+      });
+    }
   };
 
   //Cadastra usuário
@@ -66,9 +91,34 @@ export default function Home() {
   const registerUser = () => {
     validateName(state.name);
     validadeEmail(state.email);
+    validadePassword(state.password);
+    if (
+      !errorName.error_name &&
+      !errorEmail.error_email &&
+      !errorPassword.error_password
+    ) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+    }
   };
-  console.log(state);
-  console.log(state.errors);
+  const resetFields = () => {
+    setErrorName({
+      ...errorName,
+      error_name: false,
+      error_msg: ''
+    });
+    setErrorEmail({
+      ...errorEmail,
+      error_email: false,
+      error_msg: ''
+    });
+    setErrorPassword({
+      ...errorPassword,
+      error_password: false,
+      error_msg: ''
+    });
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -78,12 +128,19 @@ export default function Home() {
         <h1>Cadastro de Usuário</h1>
         <form>
           <label htmlFor="name">Nome</label>
-          <input type="text" name="name" onChange={handlerChange} id="name" />
+          <input
+            type="text"
+            name="name"
+            onChange={handlerChange}
+            onClick={resetFields}
+            id="name"
+          />
           <label htmlFor="email">E-mail</label>
           <input
             type="email"
             name="email"
             onChange={handlerChange}
+            onClick={resetFields}
             id="email"
           />
           <label htmlFor="password">Senha</label>
@@ -91,6 +148,7 @@ export default function Home() {
             type="password"
             name="password"
             onChange={handlerChange}
+            onClick={resetFields}
             id="password"
           />
           <button type="button" onClick={registerUser}>
@@ -98,13 +156,20 @@ export default function Home() {
           </button>
         </form>
 
-        {state.errors.error_name ||
-        state.errors.error_email ||
-        state.errors.error_password ? (
-          <div>
-            {state.errors.error_name && <p>{state.errors_msg.error_name}</p>}
+        {errorName.error_name ||
+        errorEmail.error_email ||
+        errorPassword.error_password ? (
+          <div className={styles.errors}>
+            {errorName.error_name && <p>{errorName.error_msg}</p>}
+            {errorEmail.error_email && <p>{errorEmail.error_msg}</p>}
+            {errorPassword.error_password && <p>{errorPassword.error_msg}</p>}
           </div>
         ) : null}
+        {success && (
+          <div className={styles.success}>
+            <p>Cadastro realizado com sucesso!</p>
+          </div>
+        )}
       </div>
     </div>
   );
